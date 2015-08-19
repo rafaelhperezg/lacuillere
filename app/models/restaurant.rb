fc7ab06956c6138427bcb370074e3fc96d662468
Rafael Perez <rapegoto@gmail.com>
2015-08-19 15:14:06 +0200

    Add validations to models. Test OK

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
+CATEGORIES=["chinese", "italian", "japanese", "french", "belgian"]
 class Restaurant < ActiveRecord::Base
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :restaurant_id, presence: true
+  validates :rating, inclusion: { in: [0,1,2,3,4,5], allow_nil: false }
 end
