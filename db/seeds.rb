26a19f080967665cbc5054f8a037e85d20219f12
Rafael Perez <rapegoto@gmail.com>
2015-08-19 15:37:20 +0200

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "french",
+    phone_number: "0983456378"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "belgian",
+    phone_number: "0683456378"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "italian",
+    phone_number: "0983456378"
+  },
+  {
+    name:         "Super sushi",
+    address:      "Bastille 75000 Paris",
+    category:     "japanese",
+    phone_number: "0882996378"
+  },
+  {
+    name:         "Chez Phu",
+    address:      "Saint Paul 75016 Paris",
+    category:     "chinese",
+    phone_number: "02345678"
+  }
+]
+
+restaurants_attributes.each { |params| Restaurant.create!(params) }
