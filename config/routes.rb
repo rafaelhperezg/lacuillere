52dd9abe4f9a9a9c600b561bc6035f6bf046355a
Rafael Perez <rapegoto@gmail.com>
2015-08-19 20:17:43 +0200

    Add reviews controller. Add reviews action and views. Update index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :restaurants, only: [ :index, :show, :new, :create]
+  resources :restaurants, only: [ :index, :show, :new, :create] do
 
+      resources :reviews, only: [:new, :create]
 
+  end
 
 
 
