e57669bb17c0a56c6e2f964332d36c36f900bad3
Rafael Perez <rapegoto@gmail.com>
2015-08-19 17:14:10 +0200

    Add New and create Actions. test OK

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,11 +5,19 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
   end
+
+private
+
+def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+end
+
 
 end
