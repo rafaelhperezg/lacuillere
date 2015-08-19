bb831f51d420c4088aebc1666bf541c73a00d40d
Rafael Perez <rapegoto@gmail.com>
2015-08-19 20:31:12 +0200

    Add link to create restaurant. Bootstrap to create restaurant. Add before... and a private method in restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [ :show ]
 
   def index
       @restaurants = Restaurant.all
@@ -14,14 +15,18 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])  #I receive the params when the user click on the link Show task
+    # line below not necessary because we have the before.... with the private method
+    # @restaurant = Restaurant.find(params[:id])  #I receive the params when the user click on the link Show task
   end
 
 private
 
-def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-end
+  def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
 end
