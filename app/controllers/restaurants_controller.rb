f008ac4a717719fe55598b6c49d6952377156fe4
Rafael Perez <rapegoto@gmail.com>
2015-08-20 10:32:01 +0200

    show reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,12 +17,13 @@ class RestaurantsController < ApplicationController
   def show
     # line below not necessary because we have the before.... with the private method
     # @restaurant = Restaurant.find(params[:id])  #I receive the params when the user click on the link Show task
+    @reviews = @restaurant.reviews
   end
 
 private
 
   def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 
   def find_restaurant
